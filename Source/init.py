f4d431a196035faae6cc109217b50f75b65623a1
MilanRegmiGitH <milanregmi561@gmail.com>
2024-06-04 17:38:08 +0545

    Added basic GUI

--- Source/init.py
+++ Source/init.py
@@ -1,7 +1,12 @@
+import tkinter as tk
+from tkinter import messagebox
+import threading
 import pyttsx3
 import speech_recognition as sr
 import requests
 import spotipy
+import re
+import sympy as sp
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 import os
@@ -73,6 +78,39 @@ def get_song_recommendations():
     results = sp.recommendations(seed_genres=['pop'], limit=5)
     recommendations = [track['name'] for track in results['tracks']]
     return recommendations
+
+def evaluate_math_expression(expression):
+    try:
+        result = sp.sympify(expression)
+        return result
+    except sp.SympifyError:
+        return "Invalid mathematical expression"
+
+def search_web(query):
+    url = f"https://api.duckduckgo.com/?q={query}&format=json"
+    response = requests.get(url)
+    data = response.json()
+    if 'AbstractText' in data and data['AbstractText']:
+        return data['AbstractText']
+    elif 'RelatedTopics' in data and data['RelatedTopics']:
+        return data['RelatedTopics'][0]['Text']
+    else:
+        return "I couldn't find any information on that."
+
+def open_application(app_name):
+    if "discord" in app_name:
+        os.system(r'start "" "%LOCALAPPDATA%\Discord\Update.exe" --processStart "Discord.exe"')
+    elif "browser" in app_name or "web browser" in app_name or "chrome" in app_name or "microsoft edge" in app_name:
+        os.system("start https://www.google.com")
+    elif "notepad" in app_name:
+        os.system("start notepad")
+    elif "viber" in app_name:
+        os.system(r'start "" "C:\Users\hp\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Viber" ')
+    elif "calculator" in app_name:
+        os.system("start calc")
+    else:
+        speak(f"Sorry, I can't open {app_name} yet.")
+
     
 
 def handle_command(command):
@@ -109,17 +147,67 @@ def handle_command(command):
     elif "recommend game" in command or "recommend games" in command or "games" in command:
         games = get_game_recommendations()
         speak("Here are some game recommendations:   " + ", ".join(games))
-    elif "stop" in command:
+    elif "open" in command:
+        app_name = command.replace("open", "").strip()
+        open_application(app_name)
+    elif "stop" in  command or "goodbye" in command or "see you" in command or "bye" in command:
         speak("Goodbye!")
         return False
     else:
-        speak("I can not do that yet, but I am learning new things every day!")
+        math_expression = re.findall(r'[\d\.\+\-\*\/\(\)]+', command)
+        if math_expression:
+            expression = ''.join(math_expression)
+            result = evaluate_math_expression(expression)
+            speak(f"The result is {result}")
+        else:
+            search_result = search_web(command)
+            speak(search_result)
     return True
 
+# if __name__ == "__main__":
+#     speak("Hello, how can I assist you today?")
+#     active = True
+#     while active:
+#         command = listen()
+#         if command:
+#             active = handle_command(command)
+
+class AwajApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Awaj - Voice Assistant")
+        self.root.geometry("400x300")
+        self.is_listening = False
+
+        self.start_button = tk.Button(root, text="Start Listening", command=self.start_listening)
+        self.start_button.pack(pady=20)
+
+        self.stop_button = tk.Button(root, text="Stop Listening", command=self.stop_listening)
+        self.stop_button.pack(pady=20)
+
+        self.output_label = tk.Label(root, text="", wraplength=300)
+        self.output_label.pack(pady=20)
+    
+    def start_listening(self):
+        self.is_listening = True
+        self.output_label.config(text="Listening....")
+        self.listen_thread = threading.Thread(target=self.listen_for_commands)
+        self.listen_thread.start()
+    
+    def stop_listening(self):
+        self.is_listening = False
+        self.output_label.config(text="Stopped Listening")
+
+    def listen_for_commands(self):
+        while self.is_listening:
+            command = listen()
+            if command:
+                self.output_label.config(text=f"You said: {command}")
+                if not handle_command(command):
+                    self.stop_listening()
+                    break
+
 if __name__ == "__main__":
-    speak("Hello, how can I assist you today?")
-    active = True
-    while active:
-        command = listen()
-        if command:
-            active = handle_command(command)
+    root = tk.Tk()
+    app = AwajApp(root)
+    root.mainloop()
